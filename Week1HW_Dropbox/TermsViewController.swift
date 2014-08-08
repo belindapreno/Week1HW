39c20ac894c1647f4e6e96fcfb2e88ef441e1752
belinda <belindapreno@gmail.com>
2014-08-08 12:20:32 -0700

    first commit

---
+++ Week1HW_Dropbox/TermsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TermsViewController.swift
+//  Week1MonHW
+//
+//  Created by Belinda Preno on 8/8/14.
+//  Copyright (c) 2014 bp. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressDoneButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
